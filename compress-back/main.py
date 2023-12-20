79e87ec310dffd624a2ba4cdcd7f91a8c7236778
aishikanandi <aishikanandi@gmail.com>
2023-12-20 17:18:05 +0530

    final

--- compress-back/main.py
+++ compress-back/main.py
@@ -10,12 +10,11 @@ from Compression.video import main_video
 import shutil
 import os
 
-
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -36,40 +35,43 @@ async def save_upload_file(upload_file: UploadFile) -> str:
     file_location = os.path.join(upload_dir, upload_file.filename)
     with open(file_location, "wb") as file_object:
         shutil.copyfileobj(upload_file.file, file_object)
-    
+
     return file_location
 
+def remove_uploaded_file(file_path: str):
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...), fileType: str = Form(...)):
-    file_path = await save_upload_file(file)
+    try:
+        file_path = await save_upload_file(file)
+        result = {}
 
-    file_path = file_path
-    file_type = fileType
-
-    if file_type == "Text":
-        return main_text(file_path)
-    
-    elif file_type == "Image":
-        return main_image(file_path)
-    
-    elif file_type == "Audio":
-        return main_audio(file_path)
-    
-    elif file_type == "Video":
-        return main_video(file_path)
-    
-    elif file_type == "Real time text":
-        return main_real(file_path)
-    
-    elif file_type == "Mixed data":
-        return main_mixed(file_path)
-    
-    elif file_type == "Data dump":
-        return main_data(file_path)
-    
-    return {"message" : "Type not found"}
+        if fileType == "Text":
+            result = main_text(file_path)
+        elif fileType == "Image":
+            result = main_image(file_path)
+        elif fileType == "Audio":
+            result = main_audio(file_path)
+        elif fileType == "Video":
+            result = main_video(file_path)
+        elif fileType == "Real time text":
+            result = main_real(file_path)
+        elif fileType == "Mixed data":
+            result = main_mixed(file_path)
+        elif fileType == "Data dump":
+            result = main_data(file_path)
+        else:
+            result = {"message": "Type not found"}
+ 
+        return result
+        
+    finally:
+        remove_uploaded_file(file_path)
+        
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
